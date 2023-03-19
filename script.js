9a1392f69a0df15848b8105e222444dac6df34d8
M4RT1N <mjlynch35@yahoo.com>
2023-03-19 13:30:21 -0500

    Added functionality

---
+++ script.js
@@ -0,0 +1,28 @@
+function SubForm (event){
+    event.preventDefault();
+    // Get the phone number input value and remove any non-digit characters
+    var phoneInput = document.getElementById('phone-number').value;
+    var phoneDigits = phoneInput.replace(/\D/g, '');
+
+    // Format the phone number as (XXX) XXX-XXXX
+    var formattedPhone = '(' + phoneDigits.substring(0, 3) + ') ' + phoneDigits.substring(3, 6) + '-' + phoneDigits.substring(6);
+
+    if (phoneInput !== ""){
+        // Set the phone number input value to the formatted phone number
+        document.getElementById('phone-number').value = formattedPhone;
+        $.ajax({
+            url:"https://api.apispreadsheets.com/data/sVhQOLQGvbj5bcaI/",
+            type:"post",
+            data:$("#myForm").serializeArray(),
+            success: function(){
+                alert("Form Data Submitted :)")
+            },
+            error: function(){
+                alert("There was an error :(")
+            }
+        });
+    }
+    else {
+        alert("Please Enter a value.");
+    }
+}
